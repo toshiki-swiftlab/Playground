d5c8ffd7e2fae4aa9226718f2841accfba9c33fa
Toshiki Takahashi <t.toshiki.app@gmail.com>
2025-05-18 22:28:37 +0900

    PersistenceController

--- Playground/PlaygroundApp.swift
+++ Playground/PlaygroundApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct PlaygroundApp: App {
+    let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
